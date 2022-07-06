1c0e2d56a5e5eecd7b3257faf9ec400a8db9c9f1
ivcp <blizujemars@gmail.com>
2022-07-06 10:47:49 +0200

    dynamic chart done

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './Bar.module.css';
 
-const Bar = () => {
+const Bar = ({ day, amount, today }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.bar}></div>
-      <label className={styles.label}>mon</label>
+      <div className={styles.bar}>
+        <div
+          className={styles.fill}
+          style={{
+            '--height': `${amount}%`,
+            backgroundColor: today ? 'var(--cyan)' : ' var(--soft-red)',
+          }}
+        >
+          <div className={styles.expense}>$55.31</div>
+        </div>
+      </div>
+      <label className={styles.label}>{day}</label>
     </div>
   );
 };
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Bar from './Bar';
+import data from '../../src/data.json';
 
 import styles from './Chart.module.css';
 
 const Chart = () => {
+  const highestAmount = Math.max(...data.map(day => day.amount));
+  const dayIndex = new Date().getDay();
+  const today = dayIndex === 0 ? 6 : dayIndex - 1;
+
   return (
     <div className={styles.chart}>
       <h1>Spending - Last 7 days</h1>
       <div className={styles.bars}>
-        <Bar />
-        <Bar />
-        <Bar />
-        <Bar />
-        <Bar />
-        <Bar />
-        <Bar />
+        {data.map((day, i) => (
+          <Bar
+            key={day.day}
+            day={day.day}
+            today={today === i}
+            amount={(day.amount * 100) / highestAmount}
+          />
+        ))}
       </div>
       <hr />
       <div>
